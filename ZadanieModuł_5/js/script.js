0b563f6a127f50068bf7067070e7922f0a4ff6d2
Adam Janas <adamjm1986@gmail.com>
2022-10-17 13:42:29 +0200

    modul5.4

--- ZadanieModuł_5/js/script.js
+++ ZadanieModuł_5/js/script.js
@@ -1,3 +1,66 @@
-let computerMove = `kamień`;
-let playerMove = "papier";
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+if(playerInput==1)
+{
+    playerMove = 'kamień';
+}
+else if(playerInput==2)
+{
+    playerMove = 'papier';
+}
+else if(playerInput==3)
+{
+    playerMove = 'nożyce';
+}
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+let computerMove = 'nieznany ruch';
+if(randomNumber==1)
+{
+    computerMove = 'kamień';
+}
+else if(randomNumber==2)
+{
+    computerMove = 'papier';
+}
+else if(randomNumber==3)
+{
+    computerMove = 'nożyce';
+}
+
+printMessage('komputer: ' + computerMove + ', gracz ' + playerMove );
+
+if(playerMove==computerMove)
+{
+    printMessage('remis');
+}
+else if(playerMove == "papier" && computerMove=="kamień")
+{
+    printMessage('wygrałeś');
+}
+else if(playerMove == "papier" && computerMove=="nożyce")
+{
+    printMessage('przegrałeś');
+}
+
+else if(playerMove == "kamień" && computerMove=="papier")
+{
+    printMessage('przegrałeś');
+}
+else if(playerMove == "kamień" && computerMove=="nożyce")
+{
+    printMessage('wygrałeś');
+}
+
+else if(playerMove == "nożyce" && computerMove=="kamień")
+{
+    printMessage('przegrałeś');
+}
+else if(playerMove == "nożyce" && computerMove=="papier")
+{
+    printMessage('wygrałeś');
+}
